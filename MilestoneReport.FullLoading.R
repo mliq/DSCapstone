cd03a3dd803827aa6b9598a990769a2936fbbc62
mliq <l12021l@gmail.com>
2014-11-16 15:24:39 -0600

    about to fix some unix count errors

---
+++ MilestoneReport.FullLoading.R
@@ -0,0 +1,202 @@
+
+# Start the clock!
+ptm <- proc.time()
+
+# SETUP #
+gc()
+setwd("C:/Users/Michael/SkyDrive/Code/GitHub/DSCapstone/Coursera-SwiftKey/final/en_US")
+library(tm)
+
+# FUNCTION DEFINITIONS #
+
+# Make Corpus, Transform, Make Trigram TDM
+makeTDM <- function(x) {
+corpus<-Corpus(VectorSource(x))
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, content_transformer(tolower))
+# corpus <- tm_map(corpus, removeWords, stopwords("english"))
+corpus <- tm_map(corpus, stemDocument)
+corpus<- tm_map(corpus,removePunctuation)
+corpus<- tm_map(corpus,removeNumbers)
+tdm<- TermDocumentMatrix(corpus)
+#tdm<-removeSparseTerms(tdm,0.97)
+return(tdm)}
+
+## DATA MUNGING ##
+
+# 1. Corpus, transformations, and TDM Creation
+#=============================================#
+
+fileMunge<- function(x) {
+text<-readLines(x)
+totalLines=length(text)
+chunkSize=20000
+chunks=totalLines/chunkSize
+remainder = chunks %% 1
+wholeChunks = chunks-remainder
+# initialize list
+output=list()
+# break file into chunks 
+i=1
+line=1
+while (i<=wholeChunks){
+end=line+chunkSize-1
+output[[i]]<-text[line:end]
+line=end+1
+i=i+1
+}
+output[[i]]<-text[line:totalLines]
+# Text Transformations to remove odd characters #
+output=lapply(output,FUN=iconv, to='ASCII', sub=' ')
+output=lapply(output,FUN= function(x) gsub("'{2}", " ",x))
+output=lapply(output,FUN= function(x) gsub("[0-9]", " ",x))
+}
+
+# Read, chunk, parse data, then make corpus, do transformations, make TDM of tri-grams:
+twit<-fileMunge("en_US.twitter.txt")
+tTDM <- makeTDM(twit)
+rm(twit)
+gc()
+
+news<-fileMunge("en_US.news.txt")
+nTDM <- makeTDM(news)
+rm(news)
+gc()
+
+blog<-fileMunge("en_US.blogs.txt")
+bTDM <- makeTDM(blog)
+rm(blog)
+gc()
+
+## Get line count
+
+tlCount<-system("wc -l en_US.twitter.txt",intern=TRUE)
+tlCount<-strsplit(tlCount," ")
+tlCount<-as.numeric(tlCount[[1]][1])
+
+nlCount<-system("wc -l en_US.news.txt",intern=TRUE)
+nlCount<-strsplit(nlCount," ")
+nlCount<-as.numeric(nlCount[[1]][1])
+
+blCount<-system("wc -l en_US.blogs.txt",intern=TRUE)
+blCount<-strsplit(blCount," ")
+blCount<-as.numeric(blCount[[1]][1])
+
+## Number of Total Words
+
+twCount<-system("wc -w en_US.twitter.txt",intern=TRUE)
+twCount<-strsplit(twCount," ")
+twCount<-as.numeric(twCount[[1]][1])
+
+nwCount<-system("wc -w en_US.news.txt",intern=TRUE)
+nwCount<-strsplit(nwCount," ")
+nwCount<-as.numeric(nwCount[[1]][1])
+
+bwCount<-system("wc -w en_US.blogs.txt",intern=TRUE)
+bwCount<-strsplit(bwCount," ")
+bwCount<-as.numeric(bwCount[[1]][1])
+
+## Get maximum line length
+
+tlmount<-system("wc -L en_US.twitter.txt",intern=TRUE)
+tlmount<-strsplit(tlmount," ")
+tlmount<-as.numeric(tlmount[[1]][1])
+
+nlmount<-system("wc -L en_US.news.txt",intern=TRUE)
+nlmount<-strsplit(nlmount," ")
+nlmount<-as.numeric(nlmount[[1]][1])
+
+blmount<-system("wc -L en_US.blogs.txt",intern=TRUE)
+blmount<-strsplit(blmount," ")
+blmount<-as.numeric(blmount[[1]][1])
+
+
+## get total unstemmed words
+system(cat en_US.twitter.txt | tr " " "\n" | sort | uniq -c | wc -l
+## Get unique words
+
+system(cat testData1.txt | tr " " "\n" | sort | uniq -c | wc -l)
+tUgrams<-dimnames(tTDM)$Terms
+bUgrams<-dimnames(bTDM)$Terms
+nUgrams<-dimnames(nTDM)$Terms
+
+### Number of Unique Words
+
+tuwCount=length(tUgrams)
+buwCount=length(bUgrams)
+nuwCount=length(nUgrams)
+
+## Get frequency table with word lengths.
+
+charFreqTable<-function(x){
+wLengths<-as.numeric((lapply(x,nchar)))
+wlFreq=lapply(min(wLengths):max(wLengths),FUN=function(x){length(which(wLengths==x))})
+counts=list(min(wLengths):max(wLengths))
+return(data.frame(Chars=(min(wLengths):max(wLengths)),Freq=unlist(wlFreq)))
+}
+
+twlFreq<-charFreqTable(tUgrams)
+bwlFreq<-charFreqTable(bUgrams)
+nwlFreq<-charFreqTable(nUgrams)
+
+## AVERAGE unique word Length
+
+tproduct=lapply(1:nrow(twlFreq),FUN=function(x){(twlFreq[x,1]*twlFreq[x,2])})
+tAvg=(sum(unlist(tproduct))/sum(twlFreq$Freq)) # total chars
+
+bproduct=lapply(1:nrow(bwlFreq),FUN=function(x){(bwlFreq[x,1]*bwlFreq[x,2])})
+bAvg=(sum(unlist(bproduct))/sum(bwlFreq$Freq)) # total chars
+
+nproduct=lapply(1:nrow(nwlFreq),FUN=function(x){(nwlFreq[x,1]*nwlFreq[x,2])})
+nAvg=(sum(unlist(nproduct))/sum(nwlFreq$Freq)) # total chars
+
+# Plot
+
+# 3 corpora in one plot barplots:
+
+## Shared parameters
+
+names=c("Twitter Corpus", "News Corpus", "Blogs Corpus")
+colors=c("cadetblue3","chocolate2","firebrick1")
+par(mai=c(1,2,1,1))
+
+## Number of Lines
+
+counts=c(tlCount,blCount,nlCount)
+bp<-barplot(counts, col=colors, main="Number of Lines", horiz=TRUE, names.arg=names, las=1, axes=FALSE)
+axis(1, at = c(0,500000,1000000,1500000,2000000), labels=c("0","500,000","1,000,000","1,500,000","2,000,000"))
+text(0,bp,prettyNum(counts,big.mark=",",scientific=F),cex=1.3,pos=4) 
+
+## Average Unique Word Length
+
+counts=c(tAvg,bAvg,nAvg)
+bp<-barplot(counts, col=colors, main="Average Unique Word Length", horiz=TRUE, names.arg=names, las=1, axes=FALSE, xlim=c(0,10))
+axis(1, at = c(0,2.5,5,7.5,10), labels=c(0,2.5,5,7.5,10))
+text(0,bp,prettyNum(counts,big.mark=",",scientific=F),cex=1.3,pos=4) 
+
+## Number of Unique Words
+counts=c(tuwCount,buwCount,nuwCount)
+bp<-barplot(counts, col=colors, main="Number of Unique Words", horiz=TRUE, names.arg=names, las=1, axes=FALSE, xlim=c(0,500000))
+axis(1, at = c(0,100000,200000,300000,400000,500000), labels=c(0,"100,000","200,000","300,000","400,000","500,000"))
+text(0,bp,prettyNum(counts,big.mark=",",scientific=F),cex=1.3,pos=4) 
+
+# Histogram for each corpus barplots:
+par(mfcol=c(3,1))
+## Line lengths in each Corpus, 3 histograms
+
+## Word length Frequency Histograms for each Corpus
+twLengths<-as.numeric((lapply(tUgrams,nchar)))
+bwLengths<-as.numeric((lapply(bUgrams,nchar)))
+nwLengths<-as.numeric((lapply(nUgrams,nchar)))
+
+hist(twLengths, col ="cadetblue3", breaks=seq(0,max(twLengths),by=1), main="Twitter Corpus Word-Lengths", xlab = "Values", cex.lab = 1.3, xlim=c(0,25))
+
+hist(nwLengths, col ="chocolate2", breaks=seq(0,max(nwLengths),by=1), main="News Corpus Word-Lengths", xlab = "Values", cex.lab = 1.3,xlim=c(0,25))
+
+hist(bwLengths, col ="firebrick1", breaks=seq(0,max(bwLengths),by=1), main="Blogs Corpus Word-Lengths", xlab = "Values", cex.lab = 1.3,xlim=c(0,25))
+
+## AVERAGE word Length
+
+
+# Stop the clock
+proc.time() - ptm # 1255.23
