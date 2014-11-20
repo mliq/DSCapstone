51876e0b90dd217de05dd8ae91cf393be5051d06
mliq <l12021l@gmail.com>
2014-11-20 17:06:40 -0600

    OK seem to have database model loading working, remember not to init it each time maybe

--- CreateSentenceDatabase.thenSameSentenceRelations.2.R
+++ CreateSentenceDatabase.thenSameSentenceRelations.2.R
@@ -4,6 +4,7 @@ ptm <- proc.time()
 
 # SETUP #
 setwd("C:/Users/Michael/SkyDrive/Code/GitHub/DSCapstone/Coursera-SwiftKey/final/en_US")
+options("max.print"=1000000)
 
 # Modify fileMunge to leave sentence separators intact and only take 10 lines for now.
 
@@ -84,43 +85,66 @@ return(tdm)}
 t.tdm <- makeTDM(twit)
 
 ##########################
-# Stop the clock
-proc.time() - ptm
 
 ######
 # Create a matrix of associations. or just straight to db.
 
-findAssocs(t.tdm,"all",0)
-
 t.ass<-lapply(dimnames(t.tdm)$Terms,FUN=function(x){findAssocs(t.tdm,x,0)})
 
-#clean out nulls.
-t.ass[grep("numeric\\(0\\)",as.character(t.ass))]=NULL 
+#clean out nulls. NOTE - why do i have stuff separated by periods though??
+t.ass[which(lapply(1:length(t.ass),FUN=function(x){is.null(dimnames(t.ass[[x]]))==1})==TRUE)]=NULL
 
+#########################################
+
+# Start the clock!
+ptm <- proc.time()
+# Create filehash database
+library(filehash)
+filehashOption("DB1")
+dbCreate("t.ass")
+db <- dbInit("t.ass", type="DB1")
+lapply(1:length(t.ass),FUN=function(x){
+if(dbExists(db, dimnames(t.ass[[x]])[[2]])==TRUE){temp<-dbFetch(db,dimnames(t.ass[[x]])[[2]])
+dbInsert(db,dimnames(t.ass[[x]])[[2]],c(dimnames(t.ass[[x]])[[1]][1:length(t.ass[[1]])],temp))
+}else{
+dbInsert(db,dimnames(t.ass[[x]])[[2]],dimnames(t.ass[[x]])[[1]][1:length(t.ass[[1]])])
+}})
+
+
+# Stop the clock
+proc.time() - ptm
+#########################################
+#########################################
+
+
+#########################################
+#########################################
+
+#t.ass[grep("numeric\\(0\\)",as.character(t.ass))]=NULL 
+#findAssocs(t.tdm,"all",0)
+
+#{dbInsert(db,dimnames(t.ass[[x]])[[2]],dimnames(t.ass[[x]])[[1]][1:length(t.ass[[1]])])})
+# OK now the problem is that they will overwrite.
+# SO i do an exists check first.
+
+lapply(t.ass,FUN=function(x)t.ass[dimnames(t.ass)==NULL]
+which(dimnames(t.ass)==NULL)
+t.ass[is.null(dimnames(t.ass))==0]
+
+t.ass[grep("character\\(0\\)",as.character(dimnames(t.ass)))]=NULL 
+
+if dimnames(t.ass[[x]])==
 
 # okay.
 #so now we can access like t.ass[word][rank of association] and get back the correlation. how do we get the word back?
 # dimnames(t.ass[[1]])[[1]] gives the matches, dimnames(t.ass[[x]][[2]]) gives the word.
 # dimnames(t.ass[[1]])[[1]][x] x is each word as a character.
 # dimnames(t.ass[[1]])[[1]][1:length(t.ass[[1]])] gives all matches as characters....
-
-# Start the clock!
-ptm <- proc.time()
-# Create filehash database
-library(filehash)
-dbCreate("mydb")
-db <- dbInit("mydb")
 # insert each unit of t.ass where [[2]] is the word, [[]]
-
-
 dbInsert(db,dimnames(t.ass[[1]])[[2]],dimnames(t.ass[[1]])[[1]][1:length(t.ass[[1]])])
-
-
-#########################################
 # yay above worked. OK we now have a basic model for making this database.
 #Next, expand it to all in our test set.
 
-lapply(1:length(t.ass),FUN=function(x){dbInsert(db,dimnames(t.ass[[x]])[[2]],dimnames(t.ass[[x]])[[1]][1:length(t.ass[[1]])])})
 # OK this worked to a point... but hit a null... at 7?
 # OK i need to remove nulls first then.
 test<-as.character(t.ass)
